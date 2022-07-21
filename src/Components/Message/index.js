b4c68f8ab793ba1865df7cfd4a8214eb7b36b715
Olatunnji TOlulope <olatunjitolulope4@gmail.com>
2022-07-21 17:28:35 +0100

    newChanges

--- src/Components/Message/index.js
+++ src/Components/Message/index.js
@@ -1,29 +1,26 @@
 import React, { useState } from "react";
-import Button from "../Button";
-import shadow from "../assets/Ellipse.png"
-import './index.scss'
+// import Button from "../Button";
+import shadow from "../assets/Ellipse.png";
+import "./index.scss";
 const Message = () => {
-  const [message] = useState("");
+  const [Name, setName] = useState('');  
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    alert('Thanks for subscribing!');
+    setName('');
+}
   return (
     <div className="blur">
       <h4>Want to be aware of all update</h4>
-      <img className="shadow5" src={shadow} alt="shadow"/>
-      <div className="text">
-
-        <div>
-          <input
-            type="text"
-            value={message}
-            placeholder=""/>
-          <p>
-            <strong>{message}</strong>
-          </p>
-        </div>
-        <Button name="Subscribe" />
-      </div>
-      <img className="shadow6" src={shadow} alt="shadow"/>
-
+      <img className="shadow5" src={shadow} alt="shadow" />
+          <form className="text" onSubmit={handleSubmit}>
+            <input type="email" value={Name} placeholder="" 
+            onChange={(e) => setName(e.target.value)}/>
+            <button type="submit" onClick={handleSubmit}>Subscribe</button>
+          </form>
+      <img className="shadow6" src={shadow} alt="shadow" />
     </div>
   );
 };
